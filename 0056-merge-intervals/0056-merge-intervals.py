d2e2323d4d7f41228b984f282433077c3c811285
litepupp <116616252+litepupp@users.noreply.github.com>
2023-05-12 23:40:16 -0500

    Time: 152 ms (56.74%), Space: 21.2 MB (9.69%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.py
+++ 0056-merge-intervals/0056-merge-intervals.py
@@ -12,11 +12,6 @@ class Solution:
             if b[1] < a[0] or b[0] > a[1]:
                 merged.append(a)
                 prev = b
-            elif b[0] < a[0]:
-                if b[1] <= a[1]:
-                    prev = [b[0], a[1]]
-                else:
-                    prev = [b[0], b[1]]
             else:
                 if b[1] > a[1]:
                     prev = [a[0], b[1]]
